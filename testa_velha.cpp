d4b8ae5c83b0956295c8dec7dc2e1cd4ce8e2bd7
lucasdbr05 <lucasgabrieljake@gmail.com>
2024-11-29 01:55:18 -0300

    :sparkles: feat: check game with result undefined

--- testa_velha.cpp
+++ testa_velha.cpp
@@ -1,6 +1,8 @@
 #include <catch2/catch_test_macros.hpp>
 #include "velha.hpp"
 
+
+
 TEST_CASE("Empty hash game", "[CheckTicTacToeResult]") {
     int hash[3][3] = {
         {0, 0, 0},
--- velha.cpp
+++ velha.cpp
@@ -1,3 +1,9 @@
+enum Value {
+    EMPTY = 0,
+    X = 1,
+    O = 2,
+};
+
 enum GameResult {
     IMPOSSIBLE = -2,
     UNDEFINED = -1,
@@ -9,5 +15,24 @@ enum GameResult {
 
 
 int CheckTicTacToeResult(int hash[3][3]) {
-    
+    int x_count = 0;
+    int y_count = 0;
+    bool flag = true;
+    if((hash[0][0]==hash[1][1] && hash[1][1]!=hash[2][2]) && hash[0][0] != 0) {
+            flag = false;
+        }
+    if((hash[0][2]==hash[1][1] && hash[1][1]!=hash[2][0]) && hash[0][0] != 0) {
+            flag = false;
+        }
+    for(int i=0; i<3; i++){
+        if((hash[i][0]==hash[i][1] && hash[i][1]!=hash[i][2]) && hash[i][0] != 0) {
+            flag = false;
+        }
+        if((hash[0][i]==hash[1][i] && hash[1][i]!=hash[2][i]) && hash[0][i] != 0) {
+            flag = false;
+        }
+    }
+
+    if(flag) return -1;
+    else return -5;
 }
