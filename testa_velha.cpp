f61280c116c8bdf1ec20b55312e10f4027a56e87
lucasdbr05 <lucasgabrieljake@gmail.com>
2024-12-01 11:46:41 -0300

    :test_tube: test: undefined game scenario

--- testa_velha.cpp
+++ testa_velha.cpp
@@ -2,17 +2,6 @@
 
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
-  
-
-
-TEST_CASE("Empty hash game with undefined result", "[CheckTicTacToeResult]" ) {
-	int hash[3][3] = {
-        {0, 0, 0},
-        {0, 0, 0},
-        {0, 0, 0},
-    };
-    REQUIRE(CheckTicTacToeResult(hash) == -1);
-} 
 
 TEST_CASE("Impossible game for X and O frequency", "[CheckTicTacToeResult]" ) {
 	int hash[3][3] = {
@@ -109,3 +98,33 @@ TEST_CASE("Tie game", "[CheckTicTacToeResult]" ) {
     };
     REQUIRE(CheckTicTacToeResult(hash) == 0);
 }
+
+TEST_CASE("Empty hash game with undefined result", "[CheckTicTacToeResult]" ) {
+	int hash[3][3] = {
+        {0, 0, 0},
+        {0, 0, 0},
+        {0, 0, 0},
+    };
+    REQUIRE(CheckTicTacToeResult(hash) == -1);
+} 
+
+TEST_CASE("Hash game with undefined result", "[CheckTicTacToeResult]" ) {
+	int hash1[3][3] = {
+        {1, 1, 2},
+        {0, 2, 0},
+        {1, 0, 0},
+    };
+    int hash2[3][3] = {
+        {2, 1, 2},
+        {1, 2, 0},
+        {1, 2, 1},
+    };
+    int hash3[3][3] = {
+        {1, 2, 1},
+        {1, 1, 0},
+        {2, 0, 2},
+    };
+    REQUIRE(CheckTicTacToeResult(hash1) == -1);
+    REQUIRE(CheckTicTacToeResult(hash2) == -1);
+    REQUIRE(CheckTicTacToeResult(hash3) == -1);
+} 
