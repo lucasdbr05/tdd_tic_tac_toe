fae6d7da358812806a692d84d7ebf7c585b91c80
lucasdbr05 <lucasgabrieljake@gmail.com>
2024-12-01 10:51:39 -0300

    :sparkles: feat: impossible game when loser plays more times than winner

--- velha.cpp
+++ velha.cpp
@@ -84,6 +84,12 @@ bool isImpossibleGame(int hash[3][3]){
     if(XIsWinner(hash) && OIsWinner(hash))
         return true;
 
+    if(XIsWinner(hash) and (frequency.y > frequency.x))
+        return true;
+
+    if(OIsWinner(hash) and (frequency.x > frequency.y))
+        return true;
+
     return false;
 }
 
@@ -106,24 +112,6 @@ int CheckTicTacToeResult(int hash[3][3] ){
     if(OIsWinner(hash)) {
         return 2;
     }
-    
-    bool flag = true;
 
-    if((hash[0][0]==hash[1][1] && hash[1][1]==hash[2][2]) && hash[0][0] != 0) {
-            flag = false;
-        }
-    if((hash[0][2]==hash[1][1] && hash[1][1]==hash[2][0]) && hash[0][0] != 0) {
-            flag = false;
-        }
-    for(int i=0; i<3; i++){
-        if((hash[i][0]==hash[i][1] && hash[i][1]==hash[i][2]) && hash[i][0] != 0) {
-            flag = false;
-        }
-        if((hash[0][i]==hash[1][i] && hash[1][i]==hash[2][i]) && hash[0][i] != 0) {
-            flag = false;
-        }
-    }
-
-    if(flag) return -1;
-    return -5; 
+    return -1;
 }
